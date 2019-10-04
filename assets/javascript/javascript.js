ffbc8b297792dc189faabf258bdd06e8bfc9b480
Eirlyse <eirlyse.s@gmail.com>
2019-10-03 21:05:09 -0700

    a whole bunch of stuff. html, css, API call creates object for each item returned, added Google Maps API

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,31 +3,63 @@ $(document).ready(function () {
 //====================================================================================================================================
 // openbrewery API call
 
+//this array is empty and gets filled on API call
+//DON'T DELETE POR FAVOR!!!
+let brewery = [];
 
-    let searchCityInput = "Phoenix"; // get value of string of city search 
-    let searchStateInput = "Arizona"; //get value of string for state search 
+$("#searchButton").on("click", function() {
 
-    let searchCity = searchCityInput.replace(" ", "_");
-    let searchState = searchStateInput.replace(" ", "_");
+    let searchCityInput = $("#searchCity").val(); // get value of string of city search 
+    let searchStateInput = $("#searchState").val(); //get value of string for state search 
 
-    console.log(searchCity)
-    console.log(searchState)
-//================================================================================================
-    let queryURL = `https://api.openbrewerydb.org/breweries?by_city${searchCity}&by_state=${searchState}`
-    // to set number of results, add &per_page=10
-    // strings must have spaces replaced with "_"
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then
-        (function (response) {
-            console.log(response);
-        },
-        function (error) {
-            console.log(error);
-        })
+    let searchCity = searchCityInput.replace(/ /g, "_");
+    let searchState = searchStateInput.replace(/ /g, "_");
+
+    console.log(searchCity, searchState);
+    
+
+        let queryURL = `https://api.openbrewerydb.org/breweries?by_city=${searchCity}&by_state=${searchState}&per_page=10`
+        // to set number of results, add &per_page=10
+        // strings must have spaces replaced with "_"
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then
+            (function (response) {
+                console.log(response);
+                for (let i = 0; i < response.length; i++) {
+                    let name = response[i].name;
+                    let street = response[i].street;
+                    let city = response[i].city;
+                    let state = response[i].state;
+                    let phone = response[i].phone;
+                    brewery[i] = new Brewery(name, street, city, state, phone);
+
+                    console.log(brewery[i]);
+                }
+                console.log(brewery);
+
+            },
+            function (error) {
+                console.log(error);
+            })
+});
 
 //====================================================================================================================================
+
+class Brewery {
+    constructor(name, street, city, state, phone, latitude, longitude) {
+        this.name = name;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.phone = phone
+        this.address = `${street},${city},${state}`;
+    };
+};
+
+
+
 
 
 });
